de0873acd158daef16724d91c6a68db3ab638cc0
Carlos Palacios <carlospalacios@Carloss-Mini.fios-router.home>
2021-11-06 07:51:42 -0400

    Added Methods

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,56 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+import Vehicle from "./vehicleBaseClass"
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
+let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage")
 console.log(v.make)
+
+
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 5
+        this.passenger = 0
+        this.numberofWheels = 4
+        this.maxSpeed = 160
+        this.fuel = 10
+        this.scheduleService = false
+    }
+    start() {
+        if (this.fuel > 0){
+            console.log("Engine started.")
+            this.started = true
+        } else {
+            return this.started = false
+            console.log("Out of Fuel.")
+        }
+    }
+    
+   loadPassenger(num) {
+       if(this.passenger < this.maximumPassengers) {
+           if((num + this.passenger) <= this.maximumPassengers){
+               this.passenger = num
+               return this.passenger
+           }
+           else {
+               console.log(this.model + " " + this.make + "Out of space to take more passengers.")
+           }
+        }
+   }    
+    scheduleService(mileage) {
+        if (this.mileage > 30000){
+            this.scheduleService == true
+            return this.scheduleService
+            //console.log("Require Maintanance")
+        }
+    }
+        
+    
+   
+}
+
+
+
+
+
+
+
